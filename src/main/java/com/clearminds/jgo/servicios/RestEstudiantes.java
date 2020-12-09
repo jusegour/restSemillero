ae32fa665e0a33a8e9b479dcee7f23d83efce48a
jusegour <jusegour2015@gmail.com>
2020-12-09 16:22:50 -0500

    Metodos rest de insertar y actualizar estudiante, listos.

--- src/main/java/com/clearminds/jgo/servicios/RestEstudiantes.java
+++ src/main/java/com/clearminds/jgo/servicios/RestEstudiantes.java
@@ -2,18 +2,37 @@ package com.clearminds.jgo.servicios;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.clearminds.jgo.dtos.Estudiante;
+import com.clearminds.jgo.excepciones.BDDException;
 
 @Path("/estudiantes")
-public class RestEstudiantes {
+public class RestEstudiantes extends ServicioEstudiante {
 
 	@Path("/insertar")
 	@POST()
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void insertar(Estudiante e){
-		System.out.println(e);
+	public void insertar(Estudiante e) {
+		try {
+			insertarEstudiante(e);
+		} catch (BDDException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	@Path("/actualizar")
+	@PUT()
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public void actualizar(Estudiante e) {
+		try {
+			actualizarEstudiante(e);
+		} catch (BDDException ex) {
+			ex.printStackTrace();
+		}
 	}
 }
